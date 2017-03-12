2ebb28b6f1ce431ec40fdf5e4ab0d8de413dbf23
mowgli-m <mayanknayyar95@gmail.com>
2017-03-13 01:50:28 +0530

    added Google Places API

--- app/src/main/java/com/example/mayank/cabservice/MainActivity.java
+++ app/src/main/java/com/example/mayank/cabservice/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -21,7 +22,9 @@ import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -29,6 +32,8 @@ import org.apache.http.message.BasicHeader;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.FileInputStream;
@@ -41,6 +46,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import java.util.logging.LogRecord;
 
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 /*
@@ -65,6 +74,9 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
     public int count = 0;
     public String UserId = null;
     MaxentTagger tagger = null;
+    public boolean dropLocationFlag = false;
+    public boolean pickUpLocationFlag = false;
+
 
 
     @Override
@@ -135,14 +147,30 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
 
     void decodeMessage(String message)
     {
+        int check = 0;
         if(flag == 0) {
             String tempDroplocation = extractLocation(message,flag);
             String[] tempDropArray = tempDroplocation.split("\\s");
-
-            if(tempDropArray.length < 3) {
+            sendBotMessage("The drop location is being verified. This may take couple of seconds");
+            if(tempDropArray.length < 7) {
                 dropLocation = tempDroplocation;
-                sendBotMessage("We detected <current location> as pickup location. \\n Do you want to use this as pickup location ? \\n Say yes or write any other location");
-                flag++;
+                sendJson(dropLocation,"NO");
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        Log.e("DLF",dropLocationFlag + "");
+                        if(dropLocationFlag) {
+                            sendBotMessage("We detected <current location> as pickup location. \n Do you want to use this as pickup location ? \n Say yes or write any other location");
+                            flag++;
+                        }
+                        else {
+                            flag = 0;
+                            sendBotMessage("We were unable to verify your drop location. Please follow the template or you maybe on slow internet connection");
+                        }
+                    }
+                }, 6000);
+
             }
             else{
                 flag = 0;
@@ -154,12 +182,10 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
             String[] tempPickUpArray;
             if(message.equals("Yes") || message.equals("yes") || message.equals("YES")) {
                 tempPickUpLocation = "current location";
-                tempPickUpArray = tempPickUpLocation.split("\\s");
-
                 pickUpLocation = tempPickUpLocation;
                 sendBotMessage("Trip Id is being generated. Thanks for your patience.");
                 sendJson(dropLocation,pickUpLocation);
-                while(UserId == null);
+
                 sendBotMessage("The drop location is: " + dropLocation + ".");
                 sendBotMessage("The pick up location is: " + pickUpLocation + ".");
                 sendBotMessage("The Id is: " + UserId + ".");
@@ -170,21 +196,34 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
             }
             else {
                 tempPickUpLocation = extractLocation(message, flag);
-
                 tempPickUpArray = tempPickUpLocation.split("\\s");
 
-                if(tempPickUpArray.length < 2)
+                if(tempPickUpArray.length < 7)
                 {
                     pickUpLocation = tempPickUpLocation;
-                    sendBotMessage("Trip Id is being generated. Thanks for your patience.");
-                    sendJson(dropLocation,pickUpLocation);
-                    while(UserId == null);
-                    sendBotMessage("The drop location is: " + dropLocation + ".");
-                    sendBotMessage("The pick up location is: " + pickUpLocation + ".");
-                    sendBotMessage("The Id is: " + UserId + ".");
-                    sendBotMessage("Thanks for booking through us.");
-                    sendBotMessage("Press 1 to make a new booking");
-                    flag ++;
+                    sendJson(pickUpLocation,"NO");
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            Log.e("PLF",pickUpLocationFlag + "");
+                            if(pickUpLocationFlag) {
+                                sendBotMessage("Trip Id is being generated. Thanks for your patience.");
+                                //sendJson(dropLocation, pickUpLocation);
+
+                                sendBotMessage("The drop location is: " + dropLocation + ".");
+                                sendBotMessage("The pick up location is: " + pickUpLocation + ".");
+                                sendBotMessage("The Id is: " + UserId + ".");
+                                sendBotMessage("Thanks for booking through us.");
+                                sendBotMessage("Press 1 to make a new booking");
+                                flag++;
+                            }
+                            else {
+                                flag = 1;
+                                sendBotMessage("We were unable to get the pick up location. Please follow the template");
+                            }
+                        }
+                    }, 6000);
 
                 }
                 else{
@@ -218,6 +257,8 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
 
     String extractLocation(String mes, int flag)
     {
+        String completeLocation = "";
+        boolean f = true;
         String posTaggedMes = postagger(mes);
         String[] posArray;
         String[] innerArray;
@@ -239,16 +280,46 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
                     if (innerArray[1].equals("TO") && innerArray[0].equals("to") && flag == 0 )
                     {
                         innerArray = posArray[i+1].split("/");
-                        if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP"))
-                            return innerArray[0];
+                        if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP") || innerArray[1].equals("CD")) {
+                            completeLocation +=innerArray[0] + " ";
+                            int j = 2;
+                            while(f && (i+j)<posArray.length)
+                              {
+                                innerArray = posArray[i+j].split("/");
+                                if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP") || innerArray[1].equals("CD")) {
+                                    completeLocation += innerArray[0] + " ";
+                                    j++;
+                                    f = true;
+                                }
+                                else
+                                    f = false;
+                            }
+                            Log.e("CL",completeLocation);
+                            return completeLocation;
+                        }
                         else
                             return "This is not the proper destination location. Please enter correct destination.";
                     }
                     if (innerArray[1].equals("IN") && innerArray[0].equals("from") && flag == 1)
                     {
                         innerArray = posArray[i+1].split("/");
-                        if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP"))
-                            return innerArray[0];
+                        if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP") || innerArray[1].equals("CD")){
+                            completeLocation +=innerArray[0] + " ";
+                            int j = 2;
+                            while(f && (i+j)<posArray.length)
+                            {
+                                innerArray = posArray[i+j].split("/");
+                                if(innerArray[1].equals("NN") || innerArray[1].equals("NNS") || innerArray[1].equals("NNP") || innerArray[1].equals("CD")) {
+                                    completeLocation += innerArray[0] + " ";
+                                    j++;
+                                    f = true;
+                                }
+                                else
+                                    f = false;
+                            }
+                            Log.e("CL",completeLocation);
+                            return completeLocation;
+                        }
                         else
                             return "This is not the proper pick up location. Please enter correct pick up location.";
                     }
@@ -407,17 +478,24 @@ public class MainActivity extends AppCompatActivity implements Network.TranferRe
     public void process(String output) {
         UserId = output;
     }
+    public void statusSearch(String status)
+    {
+        if(status.equals("OK") && flag == 0)
+            dropLocationFlag = true;
+        if(status.equals("OK") && flag == 1)
+            pickUpLocationFlag = true;
+
+    }
 }
 
 class Network extends AsyncTask<String[],Void,String[]>
 {
-
-
     public interface TranferResult {
         void process(String output);
+        void statusSearch(String status);
     }
     public  TranferResult tf = null;
-    MainActivity ma;
+
 
     public Network(TranferResult tf)
     {
@@ -425,70 +503,106 @@ class Network extends AsyncTask<String[],Void,String[]>
     }
     @Override
     protected String[] doInBackground(String[]... params) {
-        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
-        currentDateTimeString = format.format(Date.parse(currentDateTimeString));
-        Log.e("d",currentDateTimeString);
+
         String dropLocation = params[0][0];
         String pickUpLocation = params[0][1];
-        Log.e("in","thread");
-        Looper.prepare(); //For Preparing Message Pool for the child Thread
-        HttpClient client = new DefaultHttpClient();
-        HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
-        HttpResponse response;
-        JSONObject json = new JSONObject();
+        if (!pickUpLocation.equals("NO")) {
 
-        try {
-            Log.e("pic", dropLocation);
-            Log.e("pic", pickUpLocation);
-            Log.e("in","try");
-            HttpPost post = new HttpPost("http://35.166.44.35:8080/testapi/webapi/trips/trip/book");
-            Log.e("after","post");
-            json.put("t_mobileno","987898789");
-            json.put("t_bidredius","5");
-            json.put("t_bidamount","120");
-            json.put("t_status","New");
-            json.put("t_type","Bid");
-            json.put("t_bookdatetime",currentDateTimeString);
-            json.put("t_driverid","2");
-            json.put("tu_userid","7");
-            json.put("tv_vehicalid","1");
-            json.put("t_picplace", pickUpLocation);
-            json.put("t_dropplace",dropLocation );
-            json.put("t_totalfare","2042");
-            json.put("t_picdatetime","");
-            json.put("t_dropdatetime","");
-            json.put("rating","2");
+            String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            currentDateTimeString = format.format(Date.parse(currentDateTimeString));
+            Log.e("d", currentDateTimeString);
+            Log.e("in", "thread");
+            Looper.prepare(); //For Preparing Message Pool for the child Thread
+            HttpClient client = new DefaultHttpClient();
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+            HttpResponse response;
+            JSONObject json = new JSONObject();
 
-            StringEntity se = new StringEntity( json.toString());
-            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-            post.setEntity(se);
-            Log.e("after","entity");
-            response = client.execute(post);
-            Log.e("code", response.getStatusLine().getStatusCode() + "");
-            Log.e("response",response + "");
+            try {
+                Log.e("pic", dropLocation);
+                Log.e("pic", pickUpLocation);
+                Log.e("in", "try");
+                HttpPost post = new HttpPost("http://35.166.44.35:8080/testapi/webapi/trips/trip/book");
+                Log.e("after", "post");
+
+                json.put("t_mobileno", "9676190692");
+                json.put("tu_userid", "617");
+                json.put("t_bidredius", "4");
+                json.put("t_bidamount", "20");
+                json.put("tv_vehicalid", "547");
+                json.put("t_status", "New");
+                json.put("t_type", "Bid");
+                json.put("t_bookdatetime", currentDateTimeString);
+                json.put("td_driverid", "546");
+                json.put("t_picplace", pickUpLocation);
+                json.put("t_dropplace", dropLocation);
+                json.put("t_totalfare", "59.8");
+                json.put("t_picdatetime", "");
+                json.put("t_dropdatetime", "0000-00-00 00:00:00");
+                json.put("t_rating", "1");
+                json.put("t_feedback", "good");
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                Log.e("after", "entity");
+                response = client.execute(post);
+                Log.e("code", response.getStatusLine().getStatusCode() + "");
+                Log.e("response", response + "");
 
                     /*Checking response */
-            if(response!=null){
-                Log.e("in","res");
-                String result = EntityUtils.toString(response.getEntity()); //Get the data in the entity
-                Log.e("the result",result);
-                JSONObject jsonObject = new JSONObject(result);
-                if(jsonObject.getString("success").equals("true")) {
-                    JSONObject data = jsonObject.getJSONObject("data");
-                    Log.e("data ", data.getString("t_id"));
-                    tf.process(data.getString("t_id"));
+                if (response != null) {
+                    Log.e("in", "res");
+                    String result = EntityUtils.toString(response.getEntity()); //Get the data in the entity
+                    Log.e("the result", result);
+                    JSONObject jsonObject = new JSONObject(result);
+                    if (jsonObject.getString("success").equals("true")) {
+                        JSONObject data = jsonObject.getJSONObject("data");
+                        Log.e("data ", data.getString("t_id"));
+                        tf.process(data.getString("t_id"));
+                    }
+
+
                 }
 
+            } catch (Exception e) {
+                Log.e("ex", e + "");
+                e.printStackTrace();
 
             }
+        } else {
+            dropLocation = dropLocation.replaceAll("\\s", "+");
+            String URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + dropLocation + "&key=AIzaSyBb_ExzKZLFAwBDfIRy1ODLV-oLmCxeL_8";
+            HttpClient client = new DefaultHttpClient();
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 30000); //Timeout Limit
+            HttpResponse response;
+            HttpGet httpGet = new HttpGet(URL);
+            try {
+                response = client.execute(httpGet);
+                if (response != null) {
+                    Log.e("in", "res");
+                    String result = EntityUtils.toString(response.getEntity()); //Get the data in the entity
+                    Log.e("the result", result);
+                    JSONObject jsonObject = new JSONObject(result);
+                    String status = jsonObject.getString("status");
+                    Log.e("status", status);
+                    tf.statusSearch(status);
+                    /*JSONArray jsonArray = jsonObject.getJSONArray("results");
 
-        } catch(Exception e) {
-            Log.e("ex", e + "");
-            e.printStackTrace();
+                    for(int i = 0;i<jsonArray.length(); i++)
+                    {
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                        Log.e("the name", jsonObject1.getString("name"));
+                    }*/
 
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-
         return null;
     }
 }
